d32b8432d9b5759836f9705272f8c3132d65de0b
JR0-7 <jasirjaleel777@gmail.com>
2025-01-14 17:46:57 +0530

    edit:index.html

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,12 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: mode === "production" ? "/" : "/",  // Set base to "/" for custom domains like emwoods.jasir.online
 }));
